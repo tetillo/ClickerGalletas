30e3addd769753d19085884d250ac4f2a77ec967
julio.puentes <tetepuentes@gmail.com>
2025-01-22 18:03:20 +0100

    Agregar archivos de proyecto.

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+            count++;
+            GalletaCountLabel.Text = $"Galleta clickada veces: {count}";
+
+            var animation = new Animation(v => Galletabutton.Scale = v, 1, 0.8, Easing.CubicInOut);
+            animation.Commit(Galletabutton, "ScaleUp", length: 100, finished: (v, c) =>
+            {
+               
+                var resetAnimation = new Animation(v => Galletabutton.Scale = v, 0.8, 1, Easing.CubicInOut);
+                resetAnimation.Commit(Galletabutton, "ScaleDown", length: 100);
+            });
+        }
+    }
+
+
+  }
+
+
